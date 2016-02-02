b88c992d160cfbd8d93f98a9270f02c82bc4e5a3
zaki-yama <shingoyamazaki00@gmail.com>
2016-02-02 13:45:36 +0900

    delete unnecessary files

--- user-freezer/pkg/aura/HereIsYourComponentName/HereIsYourComponentNameController.js
+++
@@ -1,7 +0,0 @@
-({
-	doInit : function(component, event, helper) {
-		//hey
-    component.set("v.jqueryVersion",HereIsYourComponentName.jqueryVersion());
-    component.set("v.lodashVersion",HereIsYourComponentName.lodashVersion());
-	},
-})
